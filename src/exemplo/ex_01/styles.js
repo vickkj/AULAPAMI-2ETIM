1bf49c2975d8ccb9c5f47a90d6dadd3ef0f89822
Victor Hugo <135546677+vickkj@users.noreply.github.com>
2024-11-08 09:42:21 -0300

    victor
    fiz a atividade do dia

---
+++ src/exemplo/ex_01/styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: cemter,
+        backgroundColor: 'orange',
+        padding: 8,
+    },
+    titulo: {
+        fontSize: RFValue(20),
+        color: '#fafafa',
+        fontWeight: 'bold',
+    },
+    texto: {
+        
+    }
+});
+
+export default styles;
